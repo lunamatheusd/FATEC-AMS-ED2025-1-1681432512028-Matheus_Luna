43f9e062eeee6265c7b7e2bd2aa386f893c565df
lunamatheusd <matheusdc.luna@gmail.com>
2025-05-08 01:04:27 -0300

    Update Bubblesort.c

--- AMS-ED-2025-Entregas-S1-S2/AMS-ED-2025-Entregas-S1/AMS-ED-2025-Entregas-S1-B2/AMS-ED-2025-Entregas-S1-B2-Atividade2/Bubblesort.c
+++ AMS-ED-2025-Entregas-S1-S2/AMS-ED-2025-Entregas-S1/AMS-ED-2025-Entregas-S1-B2/AMS-ED-2025-Entregas-S1-B2-Atividade2/Bubblesort.c
@@ -1,10 +1,11 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
+/*----------------------------------------------------------------------------------*/
+/*   FATEC-São Caetano do Sul                 Estrutura de Dados                    */
+/*                         Id da Atividade: S1-B2-2                                 */
+/*             Objetivo: << Bubblesort com ordem ascendente >>                      */
+/*                                                                                  */
+/*                                Autor: Matheus David Carbone Luna                 */
+/*                                                                  Data:08/05/2025 */
+/*----------------------------------------------------------------------------------*/
 
 #include <stdio.h>
 
